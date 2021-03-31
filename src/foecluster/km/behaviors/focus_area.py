2420378f809d0fe8afd475478bb191472487168a
Khairil Yusof <khairil.yusof@gmail.com>
2021-03-31 11:00:09 +0800

    "Add behavior: FocusArea"

---
+++ src/foecluster/km/behaviors/focus_area.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from foecluster.km import _
+from plone import schema
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from Products.CMFPlone.utils import safe_hasattr
+from zope.component import adapter
+from zope.interface import Interface
+from zope.interface import implementer
+from zope.interface import provider
+
+
+class IFocusAreaMarker(Interface):
+    pass
+
+
+@provider(IFormFieldProvider)
+class IFocusArea(model.Schema):
+    """
+    """
+
+    project = schema.TextLine(
+        title=_(u'Project'),
+        description=_(u'Give in a project name'),
+        required=False,
+    )
+
+
+@implementer(IFocusArea)
+@adapter(IFocusAreaMarker)
+class FocusArea(object):
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def project(self):
+        if safe_hasattr(self.context, 'project'):
+            return self.context.project
+        return None
+
+    @project.setter
+    def project(self, value):
+        self.context.project = value
---
+++ src/foecluster/km/tests/test_behavior_focus_area.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from foecluster.km.behaviors.focus_area import IFocusAreaMarker
+from foecluster.km.testing import FOECLUSTER_KM_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class FocusAreaIntegrationTest(unittest.TestCase):
+
+    layer = FOECLUSTER_KM_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_focus_area(self):
+        behavior = getUtility(IBehavior, 'foecluster.km.focus_area')
+        self.assertEqual(
+            behavior.marker,
+            IFocusAreaMarker,
+        )
